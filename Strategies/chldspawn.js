ab3765498b8d3e367a6f7d11539e50842bff44fd
Nomad <pamirkantar21@gmail.com>
2020-05-14 17:09:12 -0700

    Kill disabled processes

--- Strategies/chldspawn.js
+++ Strategies/chldspawn.js
@@ -1,5 +1,6 @@
 const util = require('util');
-const fork = util.promisify(require('child_process').fork);
+// const fork = util.promisify(require('child_process').fork);
+const { fork } = require('child_process')
 const path = require("path")
 const numOfCpus = require("os").cpus().length;
 const userModel = require("../src/userModels"), userTradingModels = require("../src/userTradingModels");
@@ -9,7 +10,7 @@ const db = require('../src/database') //Need this defined -- don't delete it
 // let accountList = [{public: "ND9TyDMuGRE8ltbzfW", secret: "IjiDHU7TZMNNunq86iB5P01eSzl4aSuUobXC"},
 //     {public: "kkbceTwJmL51V3Gdg2", secret: "O6dVZ8PbDT3KAFNNk5OHMTee2XIWReLfgOKN"}]
 let processlist = {};
-
+var numOfProcesses = 0;
 async function main() {
     let userData = await userModel.getAllUsers();
     // Find all the users that have api keys
@@ -34,36 +35,38 @@ async function main() {
             // Settings needs to be string to be passed to a child process.
             settings = JSON.stringify(settings)
 
-            console.log(data.strategyIsEquipped, filterData[i])
             if(data.strategyIsEquipped && filterData[i].tradingEnabled){
-                console.log("Fork")
                 const child = fork('MarketMaker.js', [`${filterData[i].publicAPI}`, `${filterData[i].secretAPI}`, settings], {});
                 processlist[`${filterData[i]._id}`] = child
             }
         }(i))
     }
 
-    // setInterval(async()=>{
-    //     console.log("Check if any process needs to be killed!")
-    //     let userData = await userModel.getAllUsers();
-    //     let filterData = userData.filter((element) => {
-    //         return element.publicAPI.length > 12
-    //     })
-    //     for (let i = 0; i < filterData.length; i++) {
-    //         let data = await userTradingModels.getStrategySetting({
-    //             userId: filterData[i]._id,
-    //             strategyName: "Market Maker"
-    //         })
-    //         if(!data.strategyIsEquipped && !filterData[0].tradingEnabled){
-    //             let prcs = processlist[`${filterData[i]._id}`];
-    //             if(prcs){
-    //                 let isDead = prcs.kill();
-    //                 console.log(`Process status after kill() : ${isDead}`)
-    //             }
-    //         }
-    //     }
-    //     console.log("end of loop")
-    // }, 15000)
+    setInterval(async()=>{
+        console.log("Check if any process needs to be killed!")
+        let userData = await userModel.getAllUsers();
+        let filterData = userData.filter((element) => {
+            return element.publicAPI.length > 12
+        })
+        for (let i = 0; i < filterData.length; i++) {
+            let data = await userTradingModels.getStrategySetting({
+                userId: filterData[i]._id,
+                strategyName: "Market Maker"
+            })
+            if(!filterData[i].tradingEnabled || !data.strategyIsEquipped){
+                let prcs = processlist[`${filterData[i]._id}`];
+                if(prcs){
+                    // console.log(prcs)
+                    let isDead = prcs.kill();
+                    if(isDead){
+                        delete processlist[`${filterData[i]._id}`];
+                    }
+
+                }
+            }
+        }
+        console.log("end of loop")
+    }, 15000)
 
 
 }
